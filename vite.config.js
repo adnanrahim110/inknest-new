fc5cafb289423ced1ab39a2748ebde37f8c01a19
adnan <adnankaka.0092@gmail.com>
2025-07-02 18:56:46 +0500

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    Sitemap({
-      hostname: 'https://westernbookpublishing.com',
-      dynamicRoutes: [
-        '/about',
-        '/portfolio',
-        '/contact',
-        '/terms-and-conditions',
-        '/privacy-policy',
-      ],
-    })
   ],
   build: {
     rollupOptions: {
